8d101dfd2c916cc93f7038fe5456283b7b40ea5f
jsh123Noob <jushsolayao@gmail.com>
2025-10-27 23:09:28 +0800

    git

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Sidebar from "../components/Sidebar";
 import './Home.css';
+
 function Homepage() {
     return (
         <div>
@@ -7,8 +8,9 @@ function Homepage() {
             <div className="title-container">
                 <h2 className="greet-title">Hello User</h2>
             </div>
+
             <div className="sub-title-container">
-                <h3 className="sub-title">what's you cooking today?</h3>
+                <p className="sub-title">what's you cooking today?</p>
             </div>
         </div>
     );
