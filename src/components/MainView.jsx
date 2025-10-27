0b7c0052ae832542999ac2ad1020ddb7cbc01aca
jsh123Noob <jushsolayao@gmail.com>
2025-10-28 00:02:29 +0800

    make the toggle button static

---
+++ src/components/MainView.jsx
@@ -0,0 +1,12 @@
+import './Mainview.css';
+
+
+const MainView = () => {
+    return (
+        <div className="main-view">
+            <h1>Main View Component</h1>
+        </div>
+    )
+}
+
+export default MainView;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,28 @@
 import Sidebar from "../components/Sidebar";
+import MainView from "../components/MainView";
 import './Home.css';
 
 function Homepage() {
     return (
         <div>
-            <Sidebar />
+
             <div className="title-container">
-                <h2 className="greet-title">Hello User</h2>
+                <div className="title-left">
+                    <Sidebar /> {/* Sidebar toggle button will sit here */}
+                    <h2 className="greet-title">Hello User</h2>
+                </div>
+                <span className="material-symbols-outlined">
+                    more_vert
+                </span>
             </div>
+
 
             <div className="sub-title-container">
                 <p className="sub-title">what's you cooking today?</p>
+            </div>
+
+            <div>
+                <MainView />
             </div>
         </div>
     );
